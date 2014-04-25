946133d84870c331c1f4f8a7b680c488a2f1bc21
fskirschbaum <fskirschbaum@gmail.com>
2014-04-25 19:11:28 +0000

    Add iterable content... closes niczak/Handlebars#5

--- app.js
+++ app.js
@@ -27,15 +27,23 @@ app.configure('development', function(){
 app.get('/data', function( req, res ){
   var data = 
   { 
-    sectionOne: 
+    widgetOne: 
     {
-      template: "<h2>{{heading1}}</h2><p>{{paragraph1}}</p>",
-      content : { "heading1" : "This is the heading!", "paragraph1" : "This is a bunch of content." }
+      row: 1,
+      pos: 1,
+      col: 6,
+      type: 'listWidget',
+      template: "<h2>{{listWidgetHeading}}</h2><p>{{listWidgetParagraph}}</p>\n<ul>{{#each listWidgetList}}<li>{{this}}</li>{{/each}}</ul>",
+      content : { "listWidgetHeading" : "List Widget Heading", "listWidgetParagraph" : "This is descriptive content for the list.", "listWidgetList" : [ 'list_item_one', 'list_item_two', 'list_item_three', 'list_item_four', 'list_item_five', 'list_item_six', 'list_item_seven', 'list_item_eight', 'list_item_nine', 'list_item_ten' ] }
     },
-    sectionTwo: 
+    widgetTwo: 
     {
-      template: "<h2>{{heading2}}</h2><p>{{paragraph2}}</p>",
-      content: { "heading2" : "This is the second head!", "paragraph2" : "This is a bunch more content. So dynamic and so snazzy!" }
+      row: 1,
+      pos: 2,
+      col: 6,
+      type: "articleWidget",
+      template: "<h2>{{articleWidgetHeading}}</h2>{{#each articleWidgetParagraph}}<p>{{this}}</p>{{/each}}",
+      content: { "articleWidgetHeading" : "Article Widget Heading", "articleWidgetParagraph" : [ "Street art selfies dreamcatcher photo booth shabby chic ethnic. Flannel readymade small batch ennui mumblecore, deep v fingerstache 3 wolf moon lo-fi hoodie selfies next level Tumblr. Photo booth gastropub tattooed Marfa PBR McSweeney's. Austin flannel vinyl Brooklyn Neutra, direct trade Intelligentsia wayfarers shabby chic viral XOXO banh mi mlkshk slow-carb Marfa. Photo booth Odd Future narwhal bicycle rights shabby chic wolf Cosby sweater stumptown McSweeney's. Truffaut photo booth plaid sartorial keffiyeh actually, cray tousled lo-fi meggings kale chips retro ennui master cleanse. Austin kitsch street art chambray, readymade sartorial keytar photo booth vinyl pickled tote bag Portland.", "Sustainable slow-carb skateboard selvage Shoreditch, cardigan scenester Wes Anderson Brooklyn. Roof party tousled cliche Cosby sweater, XOXO Pinterest church-key Thundercats letterpress before they sold out gluten-free chillwave photo booth +1. Messenger bag Brooklyn XOXO 8-bit, pour-over kale chips artisan mumblecore fap salvia Pinterest put a bird on it Etsy pop-up. Cliche church-key before they sold out fingerstache Thundercats, viral retro Wes Anderson put a bird on it XOXO letterpress. Selvage flexitarian umami mlkshk, quinoa raw denim narwhal pickled. Umami photo booth banh mi you probably haven't heard of them, brunch gentrify direct trade pickled fixie synth church-key. Narwhal Tonx PBR&B seitan readymade, Neutra chillwave Schlitz lo-fi hashtag gentrify McSweeney's deep v trust fund.", "DIY wolf four loko, Carles actually Marfa church-key selvage. Pork belly tousled hashtag kitsch. Pug sustainable Banksy small batch. You probably haven't heard of them ennui plaid slow-carb. Ethnic mlkshk tattooed, sustainable lo-fi single-origin coffee artisan seitan Neutra. 90's 3 wolf moon Carles, Vice before they sold out Odd Future tote bag chia Tonx Neutra put a bird on it. Sriracha food truck dreamcatcher artisan butcher.", "Actually twee Cosby sweater Truffaut, organic McSweeney's forage tofu stumptown brunch. Bitters DIY hoodie, tousled PBR&B PBR Brooklyn quinoa 90's banh mi Odd Future disrupt flannel. Etsy mlkshk fashion axe, kitsch American Apparel cornhole normcore hashtag PBR Truffaut. Truffaut blog Blue Bottle, forage ugh swag polaroid hoodie kale chips raw denim cornhole cred fashion axe occupy. Tote bag literally yr organic, Williamsburg kitsch whatever direct trade single-origin coffee iPhone gentrify bespoke post-ironic. Wayfarers church-key art party, kogi artisan Vice occupy Wes Anderson Cosby sweater drinking vinegar Portland literally High Life selvage. Thundercats aesthetic lo-fi, Schlitz keytar master cleanse Blue Bottle craft beer." ] }
     }
   }
   res.send( data );
--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,54 @@
 $( function() {
+  
+function objectConcat() {
+  var obj = {};
+  var length = arguments.length;
+  for ( var i = 0; i < length; i++ ) {
+    for ( prop in arguments[i] ) {
+      if ( arguments[i].hasOwnProperty( prop )) {
+          obj[prop] = arguments[i][prop];
+      }
+    }
+  }
+  return obj;
+}
 
-		$( '#mainBtn' ).click( function() {
+  $( '#mainBtn' ).click( function() {
+  $.ajax( {
+      type: 'GET', 
+      url: '/data',
+      success: function( data, status, request ) {
+        console.log( 'ajax success:', data );
 
-		$.ajax( {
-				type: 'GET', 
-		    url: '/data',
-		    success: function( data, status, request ) {
-			    console.log( 'ajax success:', data );
-		
-					var templateOne = Handlebars.compile( data.sectionOne.template ),
-							templateTwo = Handlebars.compile( data.sectionTwo.template ),
-							sectionOneContent = templateOne( data.sectionOne.content ),
-							sectionTwoContent = templateTwo( data.sectionTwo.content );
+        var template = Handlebars.compile( "<div class='col-md-" + data.widgetOne.col + "'>" + data.widgetOne.template + "</div>\n <div class='col-md-" + data.widgetTwo.col + "'>" + data.widgetTwo.template + "</div>" ),
+            remoteContent = objectConcat( data.widgetOne.content, data.widgetTwo.content ),
+            content = template( remoteContent ),
+            $mainBtn = $( '#mainBtn' ),
+            $widgetContainer = $( '#widgetContainer' ),
+            $examples = $( '#examples' );
 
-					$( "#sectionOne" ).html( sectionOneContent );
-					$( "#sectionTwo" ).html( sectionTwoContent );
-					$( "#mainBtn" ).fadeOut( function() {
-						$( '#mainBtn' ).removeClass( 'btn-primary' ).addClass( 'btn-success' ).html( 'Gone!' ).fadeIn();
-					});
-		    },
-		    error: function( request, status ) {
-			    console.log( 'ajax error:', status );
-			    var data = { "heading1" : "Ajax Error", "paragraph1" : "The server respoonse is: " + status };
-			    var result = template( data );
-			    $( "#sectionOne, #sectionTwo" ).html( result );
-					$( "#mainBtn" ).fadeOut( function() {
-						$( '#mainBtn' ).removeClass( 'btn-primary' ).addClass( 'btn-danger' ).html( 'Error!' ).fadeIn();
-					});
-		    }
-		  }, 'json');
+        $mainBtn.fadeOut( function() {
+          $mainBtn.removeClass( 'btn-primary' ).addClass( 'btn-success' ).html( 'Gone!' ).fadeIn( function() {
+            setTimeout( function(){
+              $mainBtn.removeClass( 'btn-success' ).addClass( 'btn-primary' ).html( 'Again?' );
+            }, 5000 );
+          });
+        });
+        $widgetContainer.fadeIn( function() {
+          $widgetContainer.html( content );
+          $examples.hide();
+        });
+      },
+      error: function( request, status ) {
+        console.log( 'ajax error:', status );
+        var data = { "headingOne" : "Ajax Error", "paragraphOne" : "The server respoonse is: " + status, "headingTwo" : "Ajax Error", "paragraphTwo" : "The server respoonse is: " + status };
+        var result = template( data );
+        $( "#sectionOne, #sectionTwo" ).html( result );
+        $( "#mainBtn" ).fadeOut( function() {
+          $( '#mainBtn' ).removeClass( 'btn-primary' ).addClass( 'btn-danger' ).html( 'Error!' ).fadeIn();
+        });
+      }
+    }, 'json');
 	});
 
 });
