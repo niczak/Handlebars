d391e5a25908dbc14fcd54a1f45ce96611baac31
Nicholas Kreidberg <niczak@gmail.com>
2014-04-29 07:58:30 -0500

    Code clean-up
    Spaces, braces, and more, oh my!

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.configure( function() {
   app.use( express.static( path.join( __dirname, 'public' )));
 });
 
-app.configure('development', function(){
+app.configure( 'development', function() {
   app.use( express.errorHandler() );
 });
 
--- helper.js
+++ helper.js
@@ -7,16 +7,16 @@ var request = require('request'),
 	ent 	= require('ent');
 
  exports.getCurrentDate = function getCurrentDate() {
-	var today = new Date();
-	var day = today.getDate();
-	var month = today.getMonth() + 1; 
-	var year = today.getFullYear();
+	var today = new Date(),
+		day = today.getDate(),
+		month = today.getMonth() + 1,
+		year = today.getFullYear();
 
-	if(day<10) {
+	if( day < 10 ) {
 	    day = '0' + day
 	} 
 
-	if(month<10) {
+	if( month < 10 ) {
 	    month= '0' + month
 	} 
 
@@ -24,13 +24,13 @@ var request = require('request'),
 	return today; 	
  }
 
- exports.getHipsterIpsum = function getHipsterIpsum(callback) {
+ exports.getHipsterIpsum = function getHipsterIpsum( callback ) {
  	request('http://hipsterjesus.com/api?paras=4&type=hipster-centric', function(err, res, body) {
- 		if(!err && res.statusCode == 200) {
- 			var paragraphs = JSON.parse(body);
- 			paragraphArray = paragraphs.text.split("</p>");
- 			for(i = 0; i < paragraphArray.length; i++) {
- 				paragraphArray[i] = paragraphArray[i].replace('<p>', '');
+ 		if( !err && res.statusCode == 200 ) {
+ 			var paragraphs = JSON.parse( body );
+ 			paragraphArray = paragraphs.text.split( "</p>" );
+ 			for( i = 0; i < paragraphArray.length; i++ ) {
+ 				paragraphArray[i] = paragraphArray[i].replace( '<p>', '' );
  				paragraphArray[i] = ent.decode(paragraphArray[i]);
  			}
  			callback(paragraphArray);
